eb3508d4b4b8c4b32ca982b696d32485a05516aa
Sato9619 <knz2374582@stu.o-hara.ac.jp>
2024-05-16 14:58:55 +0900

    s8

--- src/bean/User.java
+++ src/bean/User.java
@@ -6,7 +6,7 @@ public class User {
 
 	/**
 <<<<<<< HEAD
-	 * ゲッター、セッター uuuuufyjyjyjyjty
+	 * ゲッター、セッター uuuuufyjyjyjyjty aaaaaaaaa
 =======
 	 * ゲッター、セッター uuuuu iiiii
 >>>>>>> branch 'master' of https://github.com/MoriTea/examtest.git
